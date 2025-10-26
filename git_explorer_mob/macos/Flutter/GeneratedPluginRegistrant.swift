57333e25ffda6cee3582fb609b562c421788988b
uncrr <workubilal@gmail.com>
2025-10-26 11:04:03 +0300

    package: window_manager, flutter_smart_dialog, fluttertoast

--- git_explorer_mob/macos/Flutter/GeneratedPluginRegistrant.swift
+++ git_explorer_mob/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,8 +9,10 @@ import desktop_webview_window
 import flutter_secure_storage_macos
 import flutter_web_auth_2
 import path_provider_foundation
+import screen_retriever_macos
 import shared_preferences_foundation
 import url_launcher_macos
+import window_manager
 import window_to_front
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,7 +20,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
   WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
