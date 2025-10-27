cd88860240811f886cde4148e30b37b188e9555e
uncrr <workubilal@gmail.com>
2025-10-27 10:59:09 +0300

    packages dependencies

--- git_explorer_mob/macos/Flutter/GeneratedPluginRegistrant.swift
+++ git_explorer_mob/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
 import url_launcher_macos
+import webview_flutter_wkwebview
 import window_manager
 import window_to_front
 
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
   WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
--- git_explorer_mob/windows/flutter/generated_plugins.cmake
+++ git_explorer_mob/windows/flutter/generated_plugins.cmake
@@ -7,6 +7,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   flutter_secure_storage_windows
   screen_retriever_windows
   url_launcher_windows
+  webview_windows
   window_manager
   window_to_front
 )
