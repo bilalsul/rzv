8c07262fc055f1b8fad1ceb99f61886558369957
uncrr <workubilal@gmail.com>
2025-11-04 16:03:35 +0300

    log: update log file names and namespace references

--- git_explorer_mob/lib/utils/get_path/log_file.dart
+++ git_explorer_mob/lib/utils/get_path/log_file.dart
@@ -5,7 +5,7 @@ import 'get_base_path.dart';
 Future<File> getLogFile() async {
   final logFileDir = await getGitExpDocumentsPath();
   final String logFilePath =
-      '$logFileDir${Platform.pathSeparator}anx_reader.log';
+      '$logFileDir${Platform.pathSeparator}git_explorer.log';
   final logFile = File(logFilePath);
   if (!logFile.existsSync()) {
     logFile.createSync();
